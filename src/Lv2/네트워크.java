0a17ccdb8762c8dd7693595fde5719e5906a0491
seongwon-ha <djfqks22@naver.com>
2023-04-03 00:06:54 +0900

    네트워크

---
+++ src/Lv2/네트워크.java
@@ -0,0 +1,28 @@
+package Lv2;
+
+public class 네트워크 {class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        for(int i = 0; i < n; i++){
+            if(!visited[i]) {
+                DFS(i, computers, visited);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public boolean[] DFS(int n, int[][] computers, boolean[] visited) {
+        visited[n] = true;
+
+        for(int j = 0; j < computers.length; j++) {
+            if(computers[n][j] == 1 && visited[j] == false && n != j){
+                visited = DFS(j, computers, visited);
+            }
+        }
+        return visited;
+    }
+}
+}
