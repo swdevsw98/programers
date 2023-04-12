29d6b07080ca6cf8393fec7bf5a1768204b28f17
seongwon-ha <djfqks22@naver.com>
2023-04-12 13:35:26 +0900

    ㄷㅓ맵게

---
+++ src/Lv2/더_맵게.java
@@ -0,0 +1,23 @@
+package Lv2;
+import java.util.*;
+
+public class 더_맵게 {
+
+    class Solution {
+        public int solution(int[] scoville, int K) {
+            int answer = 0;
+            PriorityQueue<Integer> que = new PriorityQueue<>();
+
+            for (int i = 0; i < scoville.length; i++) {
+                que.add(scoville[i]);
+            }
+            while(que.peek() < K) {
+                if (que.size() == 1)
+                    return -1;
+                que.add(que.poll() + que.poll() * 2);
+                answer++;
+            }
+            return answer;
+        }
+    }
+}
