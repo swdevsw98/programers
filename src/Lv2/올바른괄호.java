c3f30d98c93de387bd81c607e58da92c3d33436d
seongwon-ha <djfqks22@naver.com>
2023-05-20 19:49:30 +0900

    올바른 괄호

---
+++ src/Lv2/올바른괄호.java
@@ -0,0 +1,23 @@
+package Lv2;
+import java.util.*;
+
+public class 올바른괄호 {
+
+    class Solution {
+
+        boolean solution(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    stack.push('(');
+                } else if (s.charAt(i) == ')') {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+                    stack.pop();
+                }
+            }
+            return stack.isEmpty();
+        }
+    }
+}
